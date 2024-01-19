57dbbc168f28409f6ac90e18f9b6df6d5443894c
Cristhofer24 <cristhofer.elian.gramal@gmail.com>
2024-01-19 05:24:48 -0500

    mi primer comit

---
+++ JS/scrip.js
@@ -0,0 +1,120 @@
+function showBebida(){
+	//ocultar carnes
+let traer=document.getElementById("C");
+let traer1=document.getElementById("C1");
+let traer2=document.getElementById("C2");
+let traer3=document.getElementById("C3");
+traer.style.display='none';
+traer1.style.display='none';
+traer2.style.display='none';
+traer3.style.display='none';
+//Ocultar higuiene
+let traerH=document.getElementById("H");
+let traerH1=document.getElementById("H1");
+let traerH2=document.getElementById("H2");
+let traerH3=document.getElementById("H3");
+traerH.style.display='none';
+traerH1.style.display='none';
+traerH2.style.display='none';
+traerH3.style.display='none';
+//mostrar Bebidas
+let traerB=document.getElementById("Bed");
+let traerB1=document.getElementById("Bed1");
+let traerB2=document.getElementById("Bed2");
+let traerB3=document.getElementById("Bed3");
+traerB.style.display='';
+traerB1.style.display='';
+traerB2.style.display='';
+traerB3.style.display='';
+}
+
+//CARNES
+
+function showCarnes(){
+	//ocultar carnes
+let traerB=document.getElementById("Bed");
+let traerB1=document.getElementById("Bed1");
+let traerB2=document.getElementById("Bed2");
+let traerB3=document.getElementById("Bed3");
+traerB.style.display='none';
+traerB1.style.display='none';
+traerB2.style.display='none';
+traerB3.style.display='none';
+//Ocultar higuiene
+let traerH=document.getElementById("H");
+let traerH1=document.getElementById("H1");
+let traerH2=document.getElementById("H2");
+let traerH3=document.getElementById("H3");
+traerH.style.display='none';
+traerH1.style.display='none';
+traerH2.style.display='none';
+traerH3.style.display='none';
+//mostrar carnes
+let traer=document.getElementById("C");
+let traer1=document.getElementById("C1");
+let traer2=document.getElementById("C2");
+let traer3=document.getElementById("C3");
+traer.style.display='';
+traer1.style.display='';
+traer2.style.display='';
+traer3.style.display='';
+}
+//aseo
+function showAseo(){
+	//ocultar carnes
+let traerB=document.getElementById("Bed");
+let traerB1=document.getElementById("Bed1");
+let traerB2=document.getElementById("Bed2");
+let traerB3=document.getElementById("Bed3");
+traerB.style.display='none';
+traerB1.style.display='none';
+traerB2.style.display='none';
+traerB3.style.display='none';
+//Ocultar higuiene
+let traerH=document.getElementById("H");
+let traerH1=document.getElementById("H1");
+let traerH2=document.getElementById("H2");
+let traerH3=document.getElementById("H3");
+traerH.style.display='';
+traerH1.style.display='';
+traerH2.style.display='';
+traerH3.style.display='';
+//mostrar carnes
+let traer=document.getElementById("C");
+let traer1=document.getElementById("C1");
+let traer2=document.getElementById("C2");
+let traer3=document.getElementById("C3");
+traer.style.display='none';
+traer1.style.display='none';
+traer2.style.display='none';
+traer3.style.display='none';
+}
+function showtodo(){
+	//ocultar carnes
+	let traerB=document.getElementById("Bed");
+	let traerB1=document.getElementById("Bed1");
+	let traerB2=document.getElementById("Bed2");
+	let traerB3=document.getElementById("Bed3");
+	traerB.style.display='';
+	traerB1.style.display='';
+	traerB2.style.display='';
+	traerB3.style.display='';
+	//Ocultar higuiene
+	let traerH=document.getElementById("H");
+	let traerH1=document.getElementById("H1");
+	let traerH2=document.getElementById("H2");
+	let traerH3=document.getElementById("H3");
+	traerH.style.display='';
+	traerH1.style.display='';
+	traerH2.style.display='';
+	traerH3.style.display='';
+	//mostrar carnes
+	let traer=document.getElementById("C");
+	let traer1=document.getElementById("C1");
+	let traer2=document.getElementById("C2");
+	let traer3=document.getElementById("C3");
+	traer.style.display='';
+	traer1.style.display='';
+	traer2.style.display='';
+	traer3.style.display='';
+	}
